abd86c4a55d57e19595eeaec4876aedffe8ad3ed
김가원(Kimgawon) <202021000@sangmyung.kr>
2025-03-03 17:23:28 +0900

    [#2] docs : 원시 타입 및 배열
    - string, number, boolean 기본 타입 학습
    - number[], string[] 배열 타입 및 Array<T> 문법 학습
    - 타입 안정성과 유연한 제네릭 사용법 비교

---
+++ everyday_types/array_type.ts
@@ -0,0 +1,19 @@
+/**
+ * # 배열(Array)
+ */
+
+// 숫자 배열 선언(number[])
+const numbers:number[] = [1,2,3,4,5];
+console.log("Numbers array : ", numbers);
+
+// 문자열 배열 선언(string[])
+const words: string[] = ["C", "Python", "Java", "TypeScript", "JavaScript"];
+console.log("Strings array : ", words);
+
+// 배열의 요소 접근
+console.log("First Number : ", numbers[0]);  // 숫자 배열의 첫번째 요쇼에 접근
+console.log("Second String : ", words[1]);
+
+// Array<T> 문법 사용
+const moreNumbers : Array<number> = [10, 20, 30];
+console.log("More numbers array : ", moreNumbers);
---
+++ everyday_types/primitive_type.ts
@@ -0,0 +1,16 @@
+/**
+ * # 원시 타입 (Primitive Types)
+ */
+
+// 문자열(string) 타입 예제
+const message:string = "Hello, TS";
+console.log(message);
+
+// 숫자(number) 타입 예제
+const count: number = 42;
+console.log(count);
+
+// 불리언(boolean) 타입 예제
+const isDone: boolean = false;
+console.log(isDone);
+
