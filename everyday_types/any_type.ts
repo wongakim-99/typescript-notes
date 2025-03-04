0a57d6bc85c4dc8138095da3d3fe9eb21b862505
김가원(Kimgawon) <202021000@sangmyung.kr>
2025-03-04 16:10:30 +0900

    [#2] docs : noImplictAny 타입체커
    - noImplictAny : false 시 기존 자바스크립트 코드를 작성해도 컴파일 오류X
    - noImplictAny : true 시 기존 자바스크립트 코드 작성시 컴파일 오류O

--- everyday_types/any_type.ts
+++ everyday_types/any_type.ts
@@ -16,3 +16,17 @@ let num4 : any = true;
 const num5 = num1 + num2 + num3 + num4; 
 
 console.log(num5);
+
+
+
+/******************************** */
+// noImplictAny 설정 false
+
+// function add(a, b) {  -> tsconfig.json 파일의 noImplictAny 타입을 true 로 설정했을 시 -> 컴파일 오류!
+//     return a + b;
+// }
+
+const num6 = 112;
+const num7 = 10;
+
+// console.log(add(num6, num7));
