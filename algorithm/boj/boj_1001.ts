dbc5eebd5d87e31bb08f13e9e95f85d7161c70c3
김가원(Kimgawon) <202021000@sangmyung.kr>
2025-03-06 15:31:50 +0900

    [#3] 백준 알고리즘 문제를 풀기위한 세팅

--- algorithm/boj/boj_1001.ts
+++ algorithm/boj/boj_1001.ts
@@ -3,17 +3,16 @@ import * as readline from "readline";
 const rl = readline.createInterface({
     input : process.stdin,
     output : process.stdout
-})
+});
 
-rl.on('line', function(line) {
+rl.on('line', (line) => {
     //입력한 값을 처리하는 코드
     const input = line.split(' ').map(Number);
-    const a =input[0];
-    const b =input[1];
-    console.log(a+b);
-    
+    const a = input[0];
+    const b = input[1];
+    console.log(a - b);
+
     rl.close();
-  }).on("close", function() {
-    //종료 전 실행할 코드s
+}).on("close", () => {
     process.exit();
-  });
+})
