1ca8b01e2a88500f9634fb9b73e86b4aa4395c40
김가원(Kimgawon) <202021000@sangmyung.kr>
2025-03-07 11:38:31 +0900

    [#3] boj-10869 : 사칙연산

---
+++ algorithm/boj/boj_10869.ts
@@ -0,0 +1,23 @@
+import * as readline from "readline";
+
+const rl = readline.createInterface({
+    input : process.stdin,
+    output : process.stdout
+});
+
+rl.on('line', (line) => {
+    // 입력한 값을 처리하는 코드
+    const input = line.split(' ').map(Number);
+    const a = input[0];
+    const b = input[1];
+
+    console.log(a + b);
+    console.log(a - b);
+    console.log(a * b);
+    console.log(Math.floor(a / b));
+    console.log(a % b);
+
+    rl.close();
+}).on("close", () => {
+    process.exit();
+})
